1694e08d36e19b8d2e48daf24e543d78758c161f
macielcjunior <maciel_castro_junior@hotmail.com>
2020-03-16 18:09:01 -0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using customerBase.Models;
+
+namespace customerBase.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Customer(Customer customer)
+        {
+            return View(customer);
+        }
+
+        public IActionResult CustomerList()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,12 @@
+namespace customerBase.Models
+{
+    public class Customer
+    {
+        public int IdClient { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Age { get; set; }
+        public string Fone { get; set; }
+    }
+}
